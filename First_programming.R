0ece5952235ed8b252b9f9c0a29f68ee91d9bec5
Shubham Dubey <114800339+ShubhamDubey0785@users.noreply.github.com>
2024-05-13 00:27:44 +0530

    first programming

---
+++ First_programming.R
@@ -0,0 +1,18 @@
+print("Welcome to r programming language")
+5+6
+sum1<-44+11
+sum1
+print(sum1)
+demo<-"Hello duniya"
+# This is a single line comment in R programming
+demo1<-2+3;
+demo2<-5+6;
+print(demo1)
+print(demo2)
+sessionInfo()
+x<-1:10
+plot(x)
+# ?plot(x)
+# R is case sensitive language
+a<-10
+A<-11
